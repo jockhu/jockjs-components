fe4cc06daaf8138f1e2c8234c352f28f6caddbff
胡家友 <jockhu@anjuke.com>
2013-08-22 18:18:26 +0800

    rm error file

--- logger/logger.js
+++ logger/logger.js
@@ -11,11 +11,73 @@
 
 (function(J){
     var a = '.anjuke.', c = 'dev.fang', h = J.D.location.host, u = 'http://' + ( /dev|test/.test(h) ? c + a + 'test' : 'm' + a + 'com' ) + '/ts.html',
-        s = h.match(/^(\w)\.(\w+)\./);
+        s = h.match(/^(\w)\.(\w+)\./), site = s ? s[1] === 'm' ? 'm' : s[2] : 'unknow', eC = encodeURIComponent;
+
     J.add('logger', {
-        site: s ? s[1] === 'm' ? 'm' : s[2] : 'unknow',
+        site: site,
         loggerUrl:u,
         autoLogger:true,
-        onError:null
+        onError:null,
+        add: add,
+        log: log
     });
+
+    var logger = J.logger;
+
+    function log(message){
+        var m = J.isString(message) ? message : getEx(message);
+        var errorInfo = '?tp=error'
+            + '&site=' + site
+            + '&msg=' + m;
+        new Image().src = u + errorInfo;
+        logger.onError && logger.onError(m);
+    }
+
+    function getEx(ex){
+        var m = [];
+        J.each(['name','message','description','url','stack','fileName','lineNumber','number','line'], function(i, v){
+            if(v in ex){
+                if(v == 'stack'){
+                    m.push( v + ':' + eC(ex[v].split(/\n/)[0]) )
+                }else{
+                    m.push( v + ':' + eC(ex[v]) )
+                }
+            }
+        });
+        return m.join(',')
+    }
+
+    function add(instance){
+        /*for (item in instance || {}){
+            if(J.isFunction( instance[item] )){
+                applyLogger(instance, item);
+            }
+        }
+
+        function applyLogger(instance, item){
+            var method = instance[item];
+            if(method.logged) return false;
+            instance[item] = function(method){
+                return function(){
+                    try{
+                        return method.apply(this, arguments);
+                    }catch(ex){
+                        log(ex)
+                    }
+                }
+            }(method);
+            method.logged = true;
+        }*/
+    }
+
+    J.W.onerror = function(message, url, line){
+        if(J.logger.autoLogger){
+            log({
+                message:message,
+                url:url,
+                line:line
+            });
+        }
+    };
+
 })(J);
