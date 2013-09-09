66934cee0442817c14b1855455ebef6d02dacbb9
江仑 <lunjiang@anjuke.com>
2013-09-09 13:14:39 +0800

    lunjiang

---
+++ data/data.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: kathleen
+ * Date: 13-9-5
+ * Time: 下午1:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+J.add("data");
