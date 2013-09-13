3e62b5797ede0fab0496e48317597d15c44e8fa1
胡家友 <jockhu@anjuke.com>
2013-09-13 16:36:03 +0800

    add speed

--- logger/speed.js
+++ logger/speed.js
@@ -2,20 +2,74 @@
  * Aifang Javascript Framework.
  * Copyright 2012 ANJUKE Inc. All rights reserved.
  *
- * @path: logger/logger.js
+ * @path: logger/speed.js
  * @author: Jock
  * @version: 1.0.0
  * @date: 2013/07/26
  *
  */
 
+/// require('logger.logger');
+
 (function(J){
+    var logger = J.logger, win = J.W, per = win.performance || {}, tim = per.timing, isNew = J.iN || 0;
     var a = '.anjuke.', c = 'dev.fang', h = J.D.location.host, isDev =/dev|test/.test(h), u = 'http://' + ( isDev ? c + a + 'test' : 'm' + a + 'com' ) + '/ts.html',
         s = h.match(/^(\w)\.(\w+)\./), site = s ? s[1] === 'm' ? 'm' : s[2] : 'unknow', eC = encodeURIComponent;
 
+    function getSpeedUrl(pageName){
+        return logger.url + '?pn=' + pageName + '&in=' + J.iN || 0
+    }
+
+    function getSpeed(){
+        var tm = J.times, url = '&tp=speed'
+        + '&PS='+ tm.PS
+        + '&BS='+ tm.BS
+        + '&CL='+ tm.CL
+        + '&PL='+ tm.PL;
+        return url
+    }
+
+    function getTiming(){
+        var url = '&tp=timing',u=[],navigationStart = tim.navigationStart, timing = {
+            redirectTime : tim.redirectEnd - tim.redirectStart,
+            domainLookupTime : tim.domainLookupEnd - tim.domainLookupStart,
+            connectTime : tim.connectEnd - tim.connectStart,
+            requestTime : tim.responseStart - tim.requestStart,
+            responseTime : tim.responseEnd - tim.responseStart,
+            domParsingTime : tim.domInteractive - tim.domLoading,
+            firstScreenTime : tim.domInteractive - navigationStart,
+            resourcesLoadedTime : tim.loadEventStart - tim.domLoading,
+            domContentLoadedTime : tim.domContentLoadedEventStart - tim.fetchStart,
+            windowLoadedTime : tim.loadEventStart - tim.fetchStart
+        };
+
+        J.each(timing, function(i, v){
+            u.push('&'+i+'='+v);
+        });
+
+        return url + u.join('');
+    }
+
+    function speed(){
+        if(!(tim && tim.navigationStart)){
+            showInfo('Navigation Timing API is not supported by your browser.');
+            return
+        }
+
+    }
+
+    logger.speed = function(){
+
+
+        function speedSend(){
+
+        }
+
+    }
+
     J.add('logger', {
         site: site,
-        url:u,
+        loggerUrl:u,
         isDev:isDev,
         autoLogger:true,
         onError:null,
