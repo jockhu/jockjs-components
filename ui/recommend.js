f63b134390fed1b21ba4dadfd261dd6ea408e4a3
潘庆利 <qinglipan@anjuke.com>
2013-11-14 18:25:37 +0800

    推荐组件已基本完成 首页流程已走通

--- ui/recommend.js
+++ ui/recommend.js
@@ -18,54 +18,69 @@
             channel:'',
             type:'',
             box:'',
+            cont:'',
             firstpage:4,
             nextpage:10,
-            total:'',
+            total:100,
             elem:'',
             propId:'',
             inforEle:'',
-            cityAlias:'',
-            onComplete:null
+            cityAlias:J.site.info.cityAlias,
+            onComplete:null,
+            onShow:null
         }, opts, pageIndex = 0;
 
         (function () {
             opts = J.mix(defaultOptions, options || {}, true);
             getData();
-            bindEvent();
         })();
 
 
         function bindEvent() {
-             if(opts.type=='home' && opts.type=='view'){
+             if(opts.type=='home' || opts.type=='view'){
                  J.g(opts.elem).on('click',function(){
-                     pageIndex++;
+                     opts.onComplete && opts.onComplete();
+                     if(opts.type=='home'){
+                         J.g(opts.elem).s('span').eq(0).addClass('loading').html('加载中');
+                     }
+                     if (pageIndex<9){
+                         pageIndex++;
+                     }else{
+                         return;
+                     }
                      getData();
                  });
              }
-             if(opts.type='list'){
+             if(opts.type=='list'){
                  window.onscroll=function(){
-                     pageIndex++;
+                     if (pageIndex<10) pageIndex++;
                      getData();
                  };
              }
         }
 
-
         function getData() {
-            var url = bindUrl(),recomm_more= J.g(opts.elem).s('span').eq(0);
+            var url = bindUrl();
             J.get({
                 url:url,
+                timeout: 15000,
                 header:{'X-TW-HAR': 'HTML'},
                 onSuccess:function(data){
-                    if(J.g(opts.elem).getStyle('display')=='none'){
-                        J.g(opts.elem).setStyle({'display':'block'});
+                    if(J.g(opts.box).getStyle('display')=='none'){
+                        J.g(opts.box).setStyle({'display':'block'});
                     }
                     if(data!=''){
                         if(pageIndex==0){
                             showBox(data);
-                        }else if(opts.total>opts.firstpage){
-                            showLoading();
+                            if(opts.total>opts.firstpage){
+                                bindEvent();
+                                J.g(opts.elem).show();
+                            }else{
+                                J.g(opts.elem).hide();
+                            }
+                        }else{
                             showMore(data);
+                            showLoading();
                         }
 
                     }
@@ -75,6 +90,7 @@
                         J.logger.log(e);
                     }else{
                         if(opts.type=='home'){
+                            var recomm_more= J.g(opts.elem).s('span').eq(0)
                             recomm_more.html('加载失败');
                             setTimeout(function(){
                                 recomm_more.html('更多推荐');
@@ -88,7 +104,7 @@
         }
 
         function bindUrl() {
-            var url = '/' + cityAlias + '/' + opts.channel + '/recommend';
+            var url = '/' + opts.cityAlias + '/' + opts.channel + '/recommend';
             if (opts.type == "list") {
                 url += getUrlChar(url)+'from=listpage';
             } else if (opts.type == 'view') {
@@ -105,7 +121,9 @@
         }
 
         function showBox(data) {
-            J.g(opts.box).html(data);
+
+            J.g(opts.cont).html(data);
+            opts.onShow&&opts.onShow();
             showInfor();
 
         }
@@ -116,8 +134,8 @@
             }else{
                 J.g(opts.elem).hide();
             }
-            if(opts.total=100){
-
+            if(pageIndex==9){
+                J.g(opts.elem).hide();
             }
 
         }
@@ -125,39 +143,36 @@
         function showMore(data) {
             if(opts.type=='home' || opts.type=='list'){
                 var div= J.create('div');
-                div.html(data).appendTo(opts.box);
+                div.html(data).appendTo(opts.cont);
             }else if(opts.type=='view'){
                 showBox(data);
             }
+            opts.onShow&&opts.onShow();
             showInfor();
         }
 
         function showLoading() {
             if(opts.type=='home'){
-                var temp= J.g(opts.box.s('.Gb')),recomm_more= J.g(opts.elem),recomm_text=recomm_more.s('span').eq(0),fetchEnd=false;
-                recommText(pageIndex,recomm_text,recomm_more);
-                recomm_more.html('加载中').addClass('loading');
+                var recomm_more=J.g(opts.elem),recomm_text=recomm_more.s('span').eq(0),temp= J.g(opts.cont).s('.Ra'),x;
                 temp.each(function(i,v){
                     var t= v.attr('href');
-                    if(!t.match(/show=1/)) v.attr('href',t+'&show=1');
+                    if(!t.match(/show=1/)){
+                        v.attr('href',t+'&show=1');
+                    }
                 });
-                function recommText(l,j,k){
-                    var x;
-                    if(recomm_count%10 == 0 && recomm_count > 10){
-                        x = recomm_count/10;
-                    }else if(recomm_count%10 == 0 && recomm_count <= 10) {
-                        x = 0;
-                    }
-                    else{
-                        x = parseInt(recomm_count/10) + 1;
-                    }
-                    if(l <= x){
-                        j.html('更多推荐');
-                        j.removeClass('loading');
-                    }else if(l > x){
-                        k.hide();
-                    }
 
+                if(opts.total%10 == 0 && opts.total > 10){
+                    x = opts.total/10;
+                }else if(opts.total%10 == 0 && opts.total <= 10) {
+                    x = 0;
+                }
+                else{
+                    x = parseInt(opts.total/10) + 1;
+                }
+                if(pageIndex <= x){
+                    recomm_text.removeClass('loading').html('更多推荐');
+                }else if(l > x){
+                    recomm_more.hide();
                 }
             }
             if(opts.type=='list'){
@@ -173,8 +188,7 @@
 
         return {
             showBox: showBox,
-            showMore: showMore,
-            onComplete:onComplete
+            showMore: showMore
         };
     }
 
