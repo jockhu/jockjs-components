6f48adbb99eb0aa6904ffdafa09c51ad7e43a019
胡家友 <jockhu@anjuke.com>
2014-04-21 18:30:12 +0800

    fix scrollfix

--- utils/scrollfix.js
+++ utils/scrollfix.js
@@ -13,7 +13,7 @@
 (function(J) {
     "use strict";
 
-    var tm = 'touchmove', ts = 'touchstart';//, dA = (J.D||J.W)[J.aL];
+    var tm = 'touchmove', ts = 'touchstart';
 
     function fnTouchMove(e){
         var bounce;
@@ -23,16 +23,33 @@
 
     (J.D||J.W)[J.aL](tm,fnTouchMove,false);
 
+    function getFirstDom(element) {
+        for (var node = element['firstChild']; node; node = node['nextSibling']) {
+            if (node.nodeType == 1) {
+                return node;
+            }
+        }
+        return element;
+    }
+
+
     function Scrollfix(elm) {
         if(!elm) return;
         var sts;
+
         elm[J.aL](ts, function(e){
+            if(elm.scrollHeight <= elm.offsetHeight){
+                getFirstDom(elm).style.height = (elm.offsetHeight+2)+"px";
+            }
             e.stopPropagation();
             sts = elm.scrollTop;
-            if(elm.scrollTop <= 0)
+            if(elm.scrollTop == 0)
                 elm.scrollTop = 1;
-            if(sts + elm.offsetHeight >= elm.scrollHeight)
+            else if(sts + elm.offsetHeight >= elm.scrollHeight)
                 elm.scrollTop = elm.scrollHeight - elm.offsetHeight - 1;
+        }, false);
+        elm[J.aL](tm, function(e){
+            e.stopPropagation();
         }, false);
     };
 
