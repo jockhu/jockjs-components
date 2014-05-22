ce32c4b71ec1fedaf928644ff9541d9d921df075
江仑 <lunjiang@anjuke.com>
2014-05-22 17:45:45 +0800

    add document.visible

--- chat/main.js
+++ chat/main.js
@@ -60,20 +60,10 @@
 
 
         function docIsVisiable(){
-            var hidden, visibilityChange;
-            if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
-                hidden = "hidden";
-                visibilityChange = "visibilitychange";
-            } else if (typeof document.mozHidden !== "undefined") {
-                hidden = "mozHidden";
-                visibilityChange = "mozvisibilitychange";
-            } else if (typeof document.msHidden !== "undefined") {
-                hidden = "msHidden";
-                visibilityChange = "msvisibilitychange";
-            } else if (typeof document.webkitHidden !== "undefined") {
-                hidden = "webkitHidden";
-                visibilityChange = "webkitvisibilitychange";
+            if(typeof document.visible === 'undefined' || document.visible === true){
+                return true;
             }
+            return false;
         }
     }
 
