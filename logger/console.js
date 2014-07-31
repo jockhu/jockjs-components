725a2928213982f174ba7f79b24fa4b8bc657046
胡家友 <jockhu@anjuke.com>
2014-07-31 15:44:29 +0800

    modify console

--- logger/console.js
+++ logger/console.js
@@ -13,7 +13,7 @@ try {
     var cl = console;
     if (cl && cl.log) {
         cl.log("%c路有多远，只有心知道，\n最美的旅程，是不断的经历，\n坚持走下去，与梦想者同行！\n", "color:#f60");
-        cl.log("请将简历发送至%c jockhu@anjukeinc.com（ 邮件标题：“姓名-应聘XX职位-来自console” ）", "color:red");
+        cl.log("请将简历发送至%c jockhu@anjukeinc.com（ 邮件标题：“姓名-应聘XX职位-来自console” ）\n\n", "color:red");
     }
 } catch (e) {
 };
